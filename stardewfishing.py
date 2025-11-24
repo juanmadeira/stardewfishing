ce32974ceb604bb7d94083eb0583d72fffa6b4d6
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-11-24 12:24:14 -0300

    feat(game): adiciona imagens iniciais de título e botão para iniciar

---
+++ stardewfishing.py
@@ -0,0 +1,30 @@
+import graphics as gf
+from PIL import Image as PILImage
+
+win = gf.GraphWin("Stardew Fishing", 720, 420)
+
+background = gf.Image(gf.Point(360, 210), "img/background.png")
+title = PILImage.open("img/title.png")
+title = title.resize((453, 207))
+title.save("img/title-resized.png")
+title = gf.Image(gf.Point(360, 110), "img/title-resized.png")
+start = gf.Image(gf.Point(360, 375), "img/start.png")
+
+background.draw(win)
+title.draw(win)
+start.draw(win)
+
+def start_game():
+    title.undraw()
+    start.undraw()
+
+
+while True:
+    key = win.checkKey()
+    key = key.upper()
+    if key != "":
+        print(key)
+    if key == "ESCAPE" or key == "Q":
+        break
+    if key == "RETURN":
+        start_game()
